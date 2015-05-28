cf95a35b0ff0b2823533c840cd9febfe0b1d5c84
Sreejith S <sreejith.satheesh@itraveller.com>
2015-05-28 22:50:29 +0530

    pushing to server

---
+++ app/src/main/java/com/itraveller/main/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.itraveller.main;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashScreen extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, WebViewClass.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+
+}
---
+++ app/src/main/java/com/itraveller/main/WebViewClass.java
@@ -0,0 +1,24 @@
+package com.itraveller.main;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.webkit.WebView;
+
+/**
+ * Created by hckr1 on 28-05-2015.
+ */
+public class WebViewClass extends Activity {
+
+    private WebView webView;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.web_view);
+        webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://m.itraveller.com");
+
+    }
+}
